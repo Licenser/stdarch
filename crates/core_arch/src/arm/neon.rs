0989047754f7f277352c265cfc75d289d48984db
Heinz N. Gies <heinz@licenser.net>
2019-08-10 17:44:00 +0200

    Fix shr using constify

--- crates/core_arch/src/arm/neon.rs
+++ crates/core_arch/src/arm/neon.rs
@@ -1657,33 +1657,83 @@ pub unsafe fn vextq_s8(a: int8x16_t, b: int8x16_t, n: i32) -> int8x16_t {
     }
 }
 
+//immediate value: 0:16
+#[allow(unused)]
+macro_rules! constify_imm4 {
+    ($imm8:expr, $expand:ident) => {
+        #[allow(overflowing_literals)]
+        match ($imm8) & 0b1111 {
+            0 => $expand!(0),
+            1 => $expand!(1),
+            2 => $expand!(2),
+            3 => $expand!(3),
+            4 => $expand!(4),
+            5 => $expand!(5),
+            6 => $expand!(6),
+            7 => $expand!(7),
+            8 => $expand!(8),
+            9 => $expand!(9),
+            10 => $expand!(10),
+            11 => $expand!(11),
+            12 => $expand!(12),
+            13 => $expand!(13),
+            14 => $expand!(14),
+            _ => $expand!(15),
+        }
+    };
+}
+
+//immediate value: 0:7
+#[allow(unused)]
+macro_rules! constify_imm3 {
+    ($imm8:expr, $expand:ident) => {
+        #[allow(overflowing_literals)]
+        match ($imm8) & 0b111 {
+            0 => $expand!(0),
+            1 => $expand!(1),
+            2 => $expand!(2),
+            3 => $expand!(3),
+            4 => $expand!(4),
+            5 => $expand!(5),
+            6 => $expand!(6),
+            _ => $expand!(7),
+        }
+    };
+}
+
 //uint8x16_t vshrq_n_u8 (uint8x16_t a, const int n)
 #[inline]
 #[target_feature(enable = "neon")]
 #[cfg_attr(target_arch = "arm", target_feature(enable = "v7"))]
 #[cfg_attr(test, assert_instr(ushr))]
-pub unsafe fn vshrq_n_u8(a: uint8x16_t, n: i32) -> uint8x16_t {
-    if n < 0 || n > 7 {
-        unreachable_unchecked();
-    };
-    uint8x16_t(
-        a.0 >> n,
-        a.1 >> n,
-        a.2 >> n,
-        a.3 >> n,
-        a.4 >> n,
-        a.5 >> n,
-        a.6 >> n,
-        a.7 >> n,
-        a.8 >> n,
-        a.9 >> n,
-        a.10 >> n,
-        a.11 >> n,
-        a.12 >> n,
-        a.13 >> n,
-        a.14 >> n,
-        a.15 >> n,
-    )
+pub unsafe fn vshrq_n_u8(a: uint8x16_t, imm8: i32) -> uint8x16_t {
+    macro_rules! call {
+        ($imm8:expr) => {
+            if $imm8 == 0 {
+                unreachable_unchecked();
+            } else {
+                uint8x16_t(
+                    a.0 >> $imm8,
+                    a.1 >> $imm8,
+                    a.2 >> $imm8,
+                    a.3 >> $imm8,
+                    a.4 >> $imm8,
+                    a.5 >> $imm8,
+                    a.6 >> $imm8,
+                    a.7 >> $imm8,
+                    a.8 >> $imm8,
+                    a.9 >> $imm8,
+                    a.10 >> $imm8,
+                    a.11 >> $imm8,
+                    a.12 >> $imm8,
+                    a.13 >> $imm8,
+                    a.14 >> $imm8,
+                    a.15 >> $imm8,
+                )
+            }
+        };
+    }
+    constify_imm3!(imm8, call)
 }
 
 //uint8x16_t vshlq_n_u8 (uint8x16_t a, const int n)
