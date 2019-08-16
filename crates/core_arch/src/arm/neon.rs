738987f2bb92a8b632480e5791fa33d09916cf50
Heinz N. Gies <heinz@licenser.net>
2019-08-16 21:44:06 +0200

    Remove now unused import

--- crates/core_arch/src/arm/neon.rs
+++ crates/core_arch/src/arm/neon.rs
@@ -1,6 +1,6 @@
 //! ARMv7 NEON intrinsics
 
-use crate::{core_arch::simd_llvm::*, hint::unreachable_unchecked, mem::transmute, ptr};
+use crate::{core_arch::simd_llvm::*, hint::unreachable_unchecked, mem::transmute};
 #[cfg(test)]
 use stdarch_test::assert_instr;
 
